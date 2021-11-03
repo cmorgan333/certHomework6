4fd02c22cf45ecc8fe48573669e9c343f495f9ac
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-11-03 16:26:20 -0400

    JSON Loop
    JSON Loop

---
+++ app/app.js
@@ -0,0 +1,153 @@
+var TOOLS = [
+{
+    // ===TENT ONE
+    toolTitle: "Marmot Tent",
+    toolThumbImg: "tent-marmot_300x300.jpg",
+    toolFullImg: "marmot-tent.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ======TENT TWO
+    toolTitle: "Nemo Tent",
+    toolThumbImg: "tent-nemo_300x300.jpg",
+    toolFullImg: "nemo-tent.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ====TENT THREE
+    toolTitle: "Big Agnes Tent",
+    toolThumbImg: "tent-bigag_300x300.jpg",
+    toolFullImg: "bigagnes-tent.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ====STOVE ONE
+    toolTitle: "Coleman Stove",
+    toolThumbImg: "stove-coleman_300x300.jpg",
+    toolFullImg: "coleman-stove.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ====STOVE TWO
+    toolTitle: "Eureka Stove",
+    toolThumbImg: "stove-eureka_300x300.jpg",
+    toolFullImg: "eureka-stove.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ====STOCE THREE
+    toolTitle: "JetBoil Stove",
+    toolThumbImg: "stove-jetboil_300x300.jpg",
+    toolFullImg: "jetboil-stove.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+{
+    // ====SLEEPING BAG ONE
+    toolTitle: "Marmot Sleeping Bag",
+    toolThumbImg: "sleepingbag-marmot_300x300.jpg",
+    toolFullImg: "sleepingbag-marmot.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+{
+    // ====SLEEPING BAG TWO
+    toolTitle: "Nemo Sleeping Bag",
+    toolThumbImg: "sleepingbag-nemo_300x300.jpg",
+    toolFullImg: "nemo-sleepingbag.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+{
+    // ====SLEEPING BAG THREE
+    toolTitle: "Kelty Sleeping Bag",
+    toolThumbImg: "sleepingbag-kelty_300x300.jpg",
+    toolFullImg: "marmot-tent.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+{
+    // ====COT ONE
+    toolTitle: "Alps Cot",
+    toolThumbImg: "cot-alps_300x300.jpg",
+    toolFullImg: "alps-cot.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+{
+    // ====COT TWO
+    toolTitle: "Helinox Cot",
+    toolThumbImg: "cot-helinox_300x300.jpg",
+    toolFullImg: "helinox-cot.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+{
+    // ====COT THREE
+    toolTitle: "Kingdom Cot",
+    toolThumbImg: "cot-kingdom_300x300.jpg",
+    toolFullImg: "kingdom-cot.jpeg",
+    toolBriefDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi.",
+    toolDescription:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quo laudantium repellendus, voluptas modi recusandae omnis qui accusamus eos atque deleniti expedita magnam quasi perferendis? Facilis ducimus cumque nihil saepe!",
+    toolPrice: "$239.00",
+},
+
+];
+
+function loadData() {
+$.each(TOOLS, function(index, tool) {
+console.log(tool.toolTitle);
+});
+}
+
+function initListeners() {}
+
+$(document).ready(function() {
+loadData();
+    // initListeners();
+})
